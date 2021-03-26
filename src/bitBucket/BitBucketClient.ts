b2ce1f49df32ee2d7354c4649ea50e4617024ec2
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-03-26 19:44:57 +0100

    Added docs to BitBucketClient

--- src/bitBucket/BitBucketClient.ts
+++ src/bitBucket/BitBucketClient.ts
@@ -3,8 +3,20 @@ import {encode} from "base-64";
 import {join} from "path";
 import PullRequestResponse from "./response/get/PullRequestResponse";
 
+/**
+ * A client to interact with the BitBucket API
+ */
 class BitBucketClient {
 
+  /**
+   * Constructor for the client
+   *
+   * @param host The host of the BitBucket server
+   * @param user The user to log in
+   * @param token The token (or password if no token was generated)
+   * @param project The project the repository is in
+   * @param repo The repository to work with
+   */
   constructor(
       private readonly host: string,
       private readonly user: string,
@@ -13,16 +25,35 @@ class BitBucketClient {
       private readonly repo: string
   ) {}
 
+  /**
+   * Helper method to create the full api path
+   *
+   * @param path The api part in the path
+   * @returns The full path to access the api
+   * @private
+   */
   private getPath(path: string): string {
     return new URL(join("rest/api", path), this.host).toString();
   }
 
+  /**
+   * Helper method to create the necessary headers for the request
+   *
+   * @returns The necessary header for the request
+   * @private
+   */
   private getHeaders(): Headers {
     let headers = new Headers();
     headers.set("Authorization", "Basic " + encode(this.user + ":" + this.token));
     return headers;
   }
 
+  /**
+   * Helper method to access the api via get requests
+   *
+   * @param path The api part to access
+   * @private
+   */
   private async get(path: string): Promise<Response> {
     console.log(this.getPath(path));
     return await fetch(this.getPath(path), {
@@ -31,6 +62,13 @@ class BitBucketClient {
     });
   }
 
+  /**
+   * Helper method to access the api via post requests
+   *
+   * @param path The api part to access
+   * @param body The body to post onto the server
+   * @private
+   */
   private async post(path: string, body: BodyInit): Promise<Response> {
     return await fetch(this.getPath(path), {
       method: "post",
@@ -39,12 +77,15 @@ class BitBucketClient {
     });
   }
 
+  /**
+   * Fetches all open pull requests for a specific repository
+   * @returns The data for all open pull request of the specific repository
+   */
   public async fetchPullRequests(): Promise<PullRequestResponse> {
     let response = await this.get(join(
       "1.0/projects", this.project, "repos", this.repo, "pull-requests"));
     return await response.json() as PullRequestResponse;
   }
-
 }
 
 export default BitBucketClient;
