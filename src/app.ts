af54cc190331ef6e9d4660bbd972078e42e25f4b
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-04-04 00:56:32 +0200

    Upon PR update the app will check if enough approvals are there and format and push

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,8 @@ const bitBucketConfig = jsonfile.readFileSync("./bitbucketconfig.json") as {
   repo: string,
   name: string,
   email: string,
-  isUserRepo: boolean
+  isUserRepo: boolean,
+  approvalsUntilFormat: number
 };
 
 const formatterConfig = jsonfile.readFileSync("./formatterconfig.json") as {
@@ -51,19 +52,21 @@ const formatterConfig = jsonfile.readFileSync("./formatterconfig.json") as {
 
   await formatter.format("C:/Users/derPi/Google Drive/Uni/Softwareprojekt/swp2020d/client/src/main/java/de/uol/swp/client/ClientApp.java");
 
-  //bbClient.startHeartbeat();
+  bbClient.startHeartbeat();
 
   bbClient.on("heartbeat", () => {
     console.log("BB Update");
   });
 
   bbClient.on("prCreate", async (pullRequest: PullRequestData) => {
+    console.log("BB PR created");
     let diffResponse = await bbClient.fetchDiff(pullRequest.id);
     let sources: Array<string> = [];
     for (let diff of diffResponse.diffs) {
       sources.push(diff.source.toString);
     }
     let extendedSources = gitClient.extendRepoPaths(sources);
+    await gitClient.beginTransaction();
     await gitClient.checkout(pullRequest.fromRef.displayId);
     await gitClient.pull();
     let checks = await styleChecker.runChecks(extendedSources);
@@ -75,9 +78,43 @@ const formatterConfig = jsonfile.readFileSync("./formatterconfig.json") as {
       let markdownString = markdownArray.join("\n");
       await bbClient.commentPullRequest(markdownString, pullRequest.id);
     }
+    gitClient.endTransaction();
   });
 
   bbClient.on("prClose", (pullRequest: PullRequestData) => {
     console.log("BB PR closed");
-  })
+  });
+
+  bbClient.on("prUpdate", async (oldPR: PullRequestData, newPR: PullRequestData) => {
+    console.log("BB PR updated");
+
+    function getApprovalCount(pr: PullRequestData): number {
+      let approvalCount = 0;
+      for (let reviewer of pr.reviewers) {
+        if (reviewer.approved) approvalCount++;
+      }
+      return approvalCount;
+    }
+
+    if (getApprovalCount(oldPR) < bitBucketConfig.approvalsUntilFormat) {
+      if (getApprovalCount(newPR) >= bitBucketConfig.approvalsUntilFormat) {
+        console.log("formatting now!");
+        let diffResponse = await bbClient.fetchDiff(oldPR.id);
+        let sources: Array<string> = [];
+        for (let diff of diffResponse.diffs) {
+          sources.push(diff.source.toString);
+        }
+        await gitClient.beginTransaction();
+        let extendedSources = gitClient.extendRepoPaths(sources);
+        await gitClient.checkout(oldPR.fromRef.displayId);
+        await formatter.format(extendedSources);
+        console.log("formatted successfully");
+
+        await gitClient.commitAll("Auto-Reformat PR#" + oldPR.id,
+          "This action was performed automatically by a bot.");
+        await gitClient.push();
+        gitClient.endTransaction();
+      }
+    }
+  });
 })();
