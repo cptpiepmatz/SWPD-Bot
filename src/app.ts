94a003a7c5066c4896bec4746a2a06308596f567
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-05-05 00:29:22 +0200

    Included the new webhook transport into the app

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,20 @@ import GitClient from "./git/GitClient";
 import StyleChecker from "./checkstyle/StyleChecker";
 import IntelliJFormatter from "./formatter/IntelliJFormatter";
 import AwaitLock from "await-lock";
-import Logger from "./logger/Logger";
+import Logger, { setWebhookPings, setWebhookUrl } from "./logger/Logger";
 
 // The token used to log into BitBucket.
 const token = (fs.readFileSync("./.token", "utf-8") as unknown as string)
   .replace(/[\r\n\s]+/g, "");
+
+// The webhook url used to log errors in Discord.
+const webhookUrl = (fs.readFileSync("./.webhook", "utf-8") as unknown as string)
+  .replace(/[\r\n\s]+/g, "");
+
+// The config used to configure the webhook.
+const webhookConfig = jsonfile.readFileSync("./webhookconfig.json") as {
+  pingsOnError: string[]
+}
 
 // The config used to configure the repo to interact with.
 const bitBucketConfig = jsonfile.readFileSync("./bitbucketconfig.json") as {
@@ -34,6 +43,9 @@ const formatterConfig = jsonfile.readFileSync("./formatterconfig.json") as {
   // A lock to make sure only one operation modifying files is used at the same
   // time.
   const lock = new AwaitLock();
+
+  setWebhookUrl(webhookUrl);
+  setWebhookPings(webhookConfig.pingsOnError);
 
   const bbClient = new BitBucketClient(
     bitBucketConfig.host,
