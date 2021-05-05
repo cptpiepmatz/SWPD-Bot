f0cc8b30b7f62bf9c6921b016e1b93103aade5f1
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-05-05 11:41:47 +0200

    Updated PullRequestParticipantData

--- src/bitBucket/types/data/PullRequestParticipantData.ts
+++ src/bitBucket/types/data/PullRequestParticipantData.ts
@@ -4,5 +4,6 @@ export default interface PullRequestParticipantData {
   user: UserData,
   role: string | "AUTHOR" | "REVIEWER",
   approved: boolean,
-  status: string
+  status: string,
+  lastReviewedCommit?: string
 }
